ffe95ca519a8c9bb84a3cc8f7b1e414152683372
Priyam Carto <cpriyam90@gmail.com>
2022-07-27 18:00:46 -0400

    fetch function added to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,4 +44,44 @@ self.addEventListener('install', function(evt) {
   
     self.clients.claim();
   });
+
+  self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then(cache => {
+            return fetch(evt.request)
+              .then(response => {
+               
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(err => {
+            
+                return cache.match(evt.request);
+              });
+          })
+          .catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      fetch(evt.request).catch(function() {
+        return caches.match(evt.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get('accept').includes('text/html')) {
+         
+            return caches.match('/');
+          }
+        });
+      })
+    );
+  });
   
