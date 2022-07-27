f8925dae9f581d1e1efe6f029dd522fe7c157bd6
Priyam Carto <cpriyam90@gmail.com>
2022-07-27 17:02:58 -0400

    function added to upload transactions in offline mode

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,9 +10,44 @@ request.onupgradeneeded = function(event) {
   request.onerror = function(event) {
     console.log(event.target.errorCode);
   };
-  
+
   function saveRecord(transaction) {
     const expense = db.expense(['new_transaction'], 'readwrite');
     const transactionObjectStore = expense.objectStore('new_transaction');
     transactionObjectStore.add(transaction);
   }
+
+  function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+    
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+              }
+              const transaction = db.transaction(['new_transaction'], 'readwrite');
+              const transactionObjectStore = transaction.objectStore('new_transaction');
+              transactionObjectStore.clear();
+    
+              alert('All saved transactions hasve been submitted!');
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      };
+ }
+
+ window.addEventListener('online', uploadTransaction);
